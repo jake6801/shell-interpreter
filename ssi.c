ca250dacf764589216645df189c01997c2659b9e
Jake Houston <jakehouston@uvic.ca>
2024-09-23 16:20:12 -0700

    finished the changing directories, added ability to exit, starting on foreground execution

--- ssi.c
+++ ssi.c
@@ -4,9 +4,9 @@
 #include <unistd.h>
 #include <limits.h> 
 #include <string.h>
+#include <sys/wait.h>
 
 char* prompt_input(){
-    //! SHOULD MOVE THE CWD STUFF OUT SO I CAN USE IT IN change_dir() AS WELL
     // get users login, hostame, and cwd then construct and print prompt
     char cwd[PATH_MAX];
     char *login = getlogin();
@@ -36,6 +36,7 @@ char* prompt_input(){
 
 int main(int argc, char *argv[]) {
     //! need to do something for if user just presses enter and doesnt type anything 
+    //! do we need to worry about piped commands? or anything involving changing directories and running things?
     while (1) {        
         char* user_input = prompt_input();
         if(user_input == NULL){
@@ -48,13 +49,21 @@ int main(int argc, char *argv[]) {
         char *token = strtok(NULL, " ");    
         if (strcmp(first_token, "cd") == 0){
             //* CHANGING DIRECTORIES
-            printf("next token: %s\n", token);
-            //! I CAN ACTUALLY JUST PUT THE TOKEN DIRECTLY INTO chdir() instead of using this change_dir() function I made 
-            if (chdir(token) == 0){
-                printf("successfully changed directory\n");
+            // determine whether to go to home directory or somewhere else
+            if (token == NULL || strcmp(token, "~") == 0) {
+                char *home_dir = getenv("HOME");
+                if (chdir(home_dir) == 0) {
+                    printf("successfully changed directory\n");
+                } else {
+                    perror("Failed to change directory");
+                }
             } else {
-                perror("Failed to change directory");
-            }             
+                if (chdir(token) == 0){
+                    printf("successfully changed directory\n");
+                } else {
+                    perror("Failed to change directory");
+                }
+            }            
         } else if (strcmp(first_token, "bg") == 0){
             //* BACKGROUND EXECUTION 
             printf("first_token is bg\n");        
@@ -69,12 +78,35 @@ int main(int argc, char *argv[]) {
                 printf("next token: %s\n", token);
                 token = strtok(NULL, " ");
             }
+        } else if (strcmp(first_token, "exit") == 0){
+            break;
         } else {
             //* FOREGROUND EXECUTION
-            printf("first_token is a function to run\n");
-            while (token!= NULL){
-                printf("next token: %s\n", token);
-                token = strtok(NULL, " ");
+            printf("first_token is a function to run\n");      
+            pid_t pid = fork();
+            if (pid == -1) {
+                perror("Failed to fork");
+            } else if (pid == 0) {
+                // now in child process so execute the command using execvp
+                // get the args into something that execvp can take as a param COME BACK TO THIS 
+                char *args[10];
+                int i = 0;       
+                args[i++] = first_token;         
+                while (token!=NULL) {
+                    args[i++] = token;
+                    token = strtok(NULL, " ");
+                }
+                args[i] = NULL;
+                if (execvp(args[0], args) == -1) {
+                    perror("execvp failed");
+                }
+                exit(EXIT_FAILURE);
+            } else {
+                // in the parent process so wait for the child to finish process 
+                //TODO what do I do in here?
+                int status;
+                waitpid(pid, &status, 0);
+                printf("Child process finished with status: %d\n", WEXITSTATUS(status));
             }
         }       
     }
