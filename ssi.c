d4abe675fbcf0fc63de2e3b0b241d52cd2dcb25b
Jake Houston <jakehouston@uvic.ca>
2024-09-26 15:46:16 -0700

    redid bg and bg list using functions outside of main to increasse modularity

--- ssi.c
+++ ssi.c
@@ -26,6 +26,11 @@ char* prompt_input(){
     char *token;
     if(fgets(input, sizeof(input), stdin) != NULL){
         input[strcspn(input, "\n")] = '\0'; //! check I can use this, this removes newline char if present
+        // if input is empty
+        // if (strlen(input) == 0) {
+        //     return strdup("");
+        // }
+        
         // free up memory and copy input so it can be returned
         char *input_copy = malloc(strlen(input)+1);
         if(input_copy != NULL){
@@ -40,17 +45,80 @@ char* prompt_input(){
     return NULL;
 }
 
+struct bg_pro* new_bg_pro(pid_t pid, char **command) {
+    struct bg_pro* new_pro = (struct bg_pro*)malloc(sizeof(struct bg_pro));
+    new_pro->pid = pid;
+    new_pro->command = command; 
+    new_pro->next = NULL;
+    return new_pro;
+}
+
+void add_bgpro_to_list(struct bg_pro** root, pid_t pid, char **command) {
+    struct bg_pro* new_bgpro = new_bg_pro(pid, command);
+    if (*root == NULL) {
+        // list is empty, make new bg pro the root
+        *root = new_bgpro;
+    } else {
+        // add new bg pro to end of linked list 
+        struct bg_pro* node = *root;
+        while(node->next != NULL) {
+            node = node->next;
+        }
+        node->next = new_bgpro;
+    }
+}
+
+void remove_bgpro_from_list(struct bg_pro** root, pid_t pid) {
+    struct bg_pro* curr = *root;
+    struct bg_pro* prev = NULL;
+
+    // removing the root
+    if (curr != NULL && curr->pid == pid) {
+        *root = curr->next;
+        printf("%d %s has terminated\n", curr->pid, curr->command[0]); //! come back to this to print in right format so the whole command and path is displayed
+        free(curr);            
+        return;
+    }
+    // remove any other, traverse until matching pid is found 
+    while (curr != NULL && curr->pid != pid) {
+        prev = curr;
+        curr = curr->next;
+    }
+    // if curr is NULL then pid not found 
+    if (curr == NULL) {
+        printf("bg process not found so it cannot be removed\n");
+        return;
+    }
+    // otherwise remove the matching pid process        
+    prev->next = curr->next;
+    printf("%d %s has terminated\n", curr->pid, curr->command[0]); //! come back to this to print in right format so the whole command and path is displayed
+    free(curr);
+}
+
+void print_bglist(struct bg_pro* root){
+    struct bg_pro* temp = root;
+    while(temp != NULL){
+        printf("%d: path ", temp->pid);
+        for (char **user_cmd = temp->command; *user_cmd != NULL; user_cmd++) {
+            printf("%s ", *user_cmd);
+        }
+        printf("\n");
+        temp = temp->next;
+    }
+}
+
 int main(int argc, char *argv[]) {
-    //! need to do something for if user just presses enter and doesnt type anything 
     //! do we need to worry about piped commands? or anything involving changing directories and running things?
-    // struct bg_pro* root = (struct bg_pro*)malloc(sizeof(struct bg_pro));
-    struct bg_pro root;
-    root.next = NULL;
+    // struct bg_pro* root = (struct bg_pro*)malloc(sizeof(struct bg_pro));    
+    struct bg_pro* root = NULL;
     while (1) {        
+        // printf("testing testing testing\n");
         char* user_input = prompt_input();
-        if(user_input == NULL){
-            printf("something went wrong, user input is null\n");
-            return 0;
+        // printf("testing testing testing\n");
+
+        // if user input is empty, reprompt
+        if(user_input[0] == '\0'){            
+            continue;
         }
         char *first_token = strtok(user_input, " ");
         char *token = strtok(NULL, " ");    
@@ -91,44 +159,35 @@ int main(int argc, char *argv[]) {
                 token = strtok(NULL, " ");
             }
             args[i] = NULL;
-            // create the instance of the process
-            struct bg_pro* next_pro = (struct bg_pro*)malloc(sizeof(struct bg_pro));
-                next_pro->pid = pid;
-                next_pro->command = args;
-                next_pro->next = NULL;
+            
+            // create new bg_pro and add it to linked list 
+            add_bgpro_to_list(&root, pid, args);
 
             if (pid == -1) {
                 perror("Failed to fork");
             } else if (pid == 0) { 
-                // child process                                
-                    // take the process off top of list and execute then shift root down one 
+                // child process                                                    
                 if (execvp(args[0], args) == -1) {
                     perror("execvp failed");
                 }
                 exit(EXIT_FAILURE);
             } else {
                 // parent process          
-                    // check if this is the first process - if so set it to the root - if not add it to the end of the list       
-                if (root.next == NULL) { //? or should this be if pid == 0
-                    root.next = next_pro;
-                } else {
-                    struct bg_pro* node = &root;
-                    while (node->next != NULL) {
-                        node = node->next;
-                    }
-                    node->next = next_pro;                 
-                }
                 printf("starting bg process: %s with pid: %d\n", args[0], pid);
 
+                //* check for completed child processes and remove them from the linked list
+                if (root != NULL) {
+                    pid_t ter = waitpid(0, NULL, WNOHANG);
+                    if (ter > 0) {
+                        remove_bgpro_from_list(&root, ter);
+                    }
+                }
             }
             // now once a child process finishes executing, remove it from the list and print that it is finished in the formatted way specified
         } else if (strcmp(first_token, "bglist") == 0){
-            //* BACKGROUND EXECUTION LIST
-            printf("first_token is bglist\n");
-            while (token!= NULL){
-                printf("next token: %s\n", token);
-                token = strtok(NULL, " ");
-            }
+            //* BACKGROUND EXECUTION LIST            
+            //! move this out of the function and just into here once everything done but for now use it for testing bg
+            print_bglist(root);
         } else if (strcmp(first_token, "exit") == 0){
             break;
         } else {
@@ -159,6 +218,25 @@ int main(int argc, char *argv[]) {
                 // printf("Child process finished with status: %d\n", WEXITSTATUS(status));
             }
         }       
+        
+        //* check for completed child processes and remove them from the linked list
+        // if (root.next != NULL) {
+        //     printf("bglist is not empty\n");
+        //     pid_t ter = waitpid(0, NULL, WNOHANG);
+        //     if (ter > 0) {
+        //         if (root->next->pid == ter) {
+        //             printf("%d:  %s has terminated\n", root->next->pid, root->next->command[0]); //! come back to this to figure out how to print the whole command and the path
+        //             root->next = root->next->next;
+        //         } else {
+        //             struct bg_pro* node = &root;
+        //             while (node->next->pid != ter) {
+        //                 node = node->next;
+        //             }
+        //             printf("%d:  %s has terminated\n", root->pid, root->command[0]); //! come back to this to figure out how to print the whole command and the path
+        //             node->next = node->next->next;
+        //         }
+        //     }
+        // }
     }
     return 0;
 }
